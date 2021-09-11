cf81ce56cb894d3a85a20fd0431dfaf7dd93bbcb
jyll sarta <illstarred.dive@gmail.com>
2021-09-11 23:38:33 +0900

    mix init

---
+++ lib/elixirika_masterdata.ex
@@ -0,0 +1,18 @@
+defmodule ElixirikaMasterdata do
+  @moduledoc """
+  Documentation for `ElixirikaMasterdata`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirikaMasterdata.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixirika_masterdata_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirikaMasterdataTest do
+  use ExUnit.Case
+  doctest ElixirikaMasterdata
+
+  test "greets the world" do
+    assert ElixirikaMasterdata.hello() == :world
+  end
+end
